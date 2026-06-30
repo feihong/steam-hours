6088078038e805d8bacb283431ea1f45997ecb74
Feihong Hsu <feihong.hsu@gmail.com>
2026-06-29 23:36:05 -0500

    Make report mobile-friendly

--- compute_steam_hours.py
+++ compute_steam_hours.py
@@ -132,6 +132,7 @@ def layout(content):
     return html[
         head[
             meta(charset='utf-8'),
+            meta(name='viewport', content='width=device-width,initial-scale=1'),
             title[title_text],
         ],
         body[
